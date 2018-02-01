3756a85d0bca31e07dc656b099894191b671a879
Zohaib Rauf <zabirauf@gmail.com>
2018-02-01 00:15:49 -0800

    Fixed the scripts in the contract package Added a contract util class that uses truffle-contract

--- packages/contracts/test/ts/contracts/UpdatableContent.test.ts
+++ packages/contracts/test/ts/contracts/UpdatableContent.test.ts
@@ -1,33 +1,50 @@
-
 import Artifacts from '../../../utils/artifacts';
 import chaiSetup from '../../../utils/chaiSetup';
 import Web3 = require('web3');
 import * as chai from 'chai';
 import { promisify } from '@0xproject/utils';
 
-const { UpdatableContent } = new Artifacts(artifacts);
-
 chaiSetup.configure();
 const expect = chai.expect;
 const web3: Web3 = (global as any).web3;
 
+const { UpdatableContent } = new Artifacts(artifacts);
+
 contract('UpdatableContent', (accounts: string[]) => {
     const account = accounts[0];
 
-    describe('changeContent', () => {
+    describe('constructor', () => {
 
-        it('should create', async () => {
+        it('should create content', async () => {
             // Arrange
             const contentAddress = 'test1';
-            const content = await UpdatableContent.new(contentAddress, {from: account});
-            const newContentAddress = 'test2';
 
             // Act
-            await promisify(UpdatableContent.changeContent)(newContentAddress, {from: account});
+            const content = await UpdatableContent.new(contentAddress, {from: account});
 
             // Assert
             const assignedContentAddress = await content.contentAddress.call();
             expect(assignedContentAddress).to.equal(contentAddress);
+
+            const owner = await content.owner.call();
+            expect(owner).to.equal(account);
+        });
+    });
+
+    describe('changeContent', () => {
+
+        it('should change content address', async () => {
+            // Arrange
+            const contentAddress = 'test1';
+            const content = await UpdatableContent.new(contentAddress, {from: account});
+            const newContentAddress = 'test2';
+            
+            // Act
+            await promisify<void>(content.changeContent)(newContentAddress, {from: account});
+
+            // Assert
+            const assignedContentAddress = await content.contentAddress.call();
+            expect(assignedContentAddress).to.equal(newContentAddress);
         });
     });
 });
--- packages/contracts/truffle.js
+++ packages/contracts/truffle.js
@@ -6,6 +6,7 @@ module.exports = {
           host: "localhost",
           port: 7545,
           network_id: "*", // Match any network id
+          gas: 4612388,
       },
       kovan: {
           host: "localhost",

72026e94fa53f21aedc8dfaa58efe323a40e2062
Zohaib Rauf <zabirauf@gmail.com>
2018-01-31 23:36:57 -0800

    Added a test for UpdatableContent

--- packages/contracts/contracts/content/IterativeContent.sol
+++ packages/contracts/contracts/content/IterativeContent.sol
@@ -2,10 +2,18 @@ pragma solidity ^0.4.18;
 
 import './UpdatableContent.sol';
 
+/**
+ * @title Iterative Content
+ * @dev The IterativeContent along with allowing you to change offchain content address also keeps track of older content addresses
+ */
 contract IterativeContent is UpdatableContent {
 
     string[] public iterations;
 
+    /**
+     * @dev It changes the offchain content address
+     * @param _contentAddress A non-empty string that represents offchain content address
+     */
     function changeContent(string _contentAddress) onlyOwner public {
         super.changeContent(_contentAddress);
         iterations.push(_contentAddress);

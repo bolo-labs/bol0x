3858f527681c0e1e91b935ed5dee18edcf61fdd5
Zohaib Rauf <zabirauf@gmail.com>
2018-02-02 23:43:45 -0800

    Changed the name in Entity to be an offchain identifier

--- packages/contracts/contracts/entity/Entity.sol
+++ packages/contracts/contracts/entity/Entity.sol
@@ -4,17 +4,40 @@ pragma solidity ^0.4.18;
 import '../utils/Ownable.sol';
 import '../utils/StringUtils.sol';
 
+/**
+ * @title Entity
+ * @dev The entity that represents a person or an organization
+ */
 contract Entity is Ownable {
 
-    string public name;
+    /**
+     * @dev The off chain identity provider if the entity want to
+     * have a known identity.
+     */
+    enum IdentityProvider {
+        ETH_ADDR,
+        KEY_BASE,
+        NAME,
+        OTHER,
+        TWITTER_HANDLE
+    }
+
+    /**
+     * @dev The offchain identifier that corresponds to the identity provider.
+     */
+    string public identifier;
+
+    IdentityProvider public identityProvider;
 
     function Entity(
-        string _name)
+        IdentityProvider _identityProvider,
+        string _identifier)
         Ownable()
         public
     {
-        require(StringUtils.isNotEmpty(_name));
+        require(StringUtils.isNotEmpty(_identifier));
 
-        name = _name;
+        identityProvider = _identityProvider;
+        identifier = _identifier;
     }
 }

e4fdb8875e7c5a8282bb05cb3b1837f5a339baae
Zohaib Rauf <zabirauf@gmail.com>
2018-03-01 21:57:48 -0800

    Added prettier

--- packages/bol0x.js/src/utils/constants.ts
+++ packages/bol0x.js/src/utils/constants.ts
@@ -2,7 +2,7 @@ import { BigNumber } from '@0xproject/utils';
 
 export default {
     NULL_ADDRESS: '0x0000000000000000000000000000000000000000',
-    TESTRPC_NETWORK_ID: 5777,
+    TESTRPC_NETWORK_ID: 50,
     MAX_DIGITS_IN_UNSIGNED_256_INT: 78,
     INVALID_JUMP_PATTERN: 'invalid JUMP at',
     OUT_OF_GAS_PATTERN: 'out of gas',
--- packages/bol0x.js/test/BoloEx.test.ts
+++ packages/bol0x.js/test/BoloEx.test.ts
@@ -5,17 +5,22 @@ import { BigNumber } from '@0xproject/utils';
 import { web3Factory } from '@0xproject/dev-utils';
 import constants from '../src/utils/constants';
 
+chaiSetup.configure();
+const expect = chai.expect;
+
 describe('BoloEx library', () => {
     const web3 = web3Factory.create();
     const config = {
         networkId: constants.TESTRPC_NETWORK_ID,
     };
-    const boloEx = new BoloEx(web3.currentProvider, config);
 
     it('test for true', async () => {
-        const something = await boloEx.createContentWrapperAsync(
+        const boloEx = new BoloEx(web3.currentProvider, config);
+        const contentContract = await boloEx.createContentWrapperAsync(
             'Content',
             'test'
         );
+
+        expect(contentContract).not.to.be.null();
     });
 });
